182446a0e833cfe0e0cfd9d49e018ec9305077a0
Nandan Pandey <nandan.pandey.noddy@gmail.com>
2016-07-04 22:50:16 +0530

    first commit

---
+++ config/passport.js
@@ -0,0 +1,121 @@
+const request = require('request');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const OpenIDStrategy = require('passport-openid').Strategy;
+
+
+var User = require('../models/User');
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// Sign in with Email and Password
+passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
+  User.findOne({ email: email }, function(err, user) {
+    if (!user) {
+      return done(null, false, { msg: 'The email address ' + email + ' is not associated with any account. ' +
+      'Double-check your email address and try again.' });
+    }
+    user.comparePassword(password, function(err, isMatch) {
+      if (!isMatch) {
+        return done(null, false, { msg: 'Invalid email or password' });
+      }
+      return done(null, user);
+    });
+  });
+}));
+
+// Sign in with Google
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_ID,
+  clientSecret: process.env.GOOGLE_SECRET,
+  callbackURL: '/auth/google/callback',
+  passReqToCallback: true
+}, function(req, accessToken, refreshToken, profile, done) {
+  if (req.user) {
+    User.findOne({ google: profile.id }, function(err, user) {
+      if (user) {
+        req.flash('error', { msg: 'There is already an existing account linked with Google that belongs to you.' });
+      } else {
+        User.findById(req.user.id, function(err, user) {
+          user.name = user.name || profile.displayName;
+          user.gender = user.gender || profile._json.gender;
+          user.picture = user.picture || profile._json.image.url;
+          user.google = profile.id;
+          user.save(function(err) {
+            req.flash('success', { msg: 'Your Google account has been linked.' });
+            done(err, user);
+          });
+        });
+      }
+    });
+  } else {
+    User.findOne({ google: profile.id }, function(err, user) {
+      if (user) {
+        return done(null, user);
+      }
+      User.findOne({ email: profile.emails[0].value }, function(err, user) {
+        if (user) {
+          req.flash('error', { msg: user.email + ' is already associated with another account.' });
+          done(err);
+        } else {
+          var newUser = new User({
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            gender: profile._json.gender,
+            location: profile._json.location,
+            picture: profile._json.image.url,
+            google: profile.id
+          });
+          newUser.save(function(err) {
+            done(err, newUser);
+          });
+        }
+      });
+    });
+  }
+}));
+
+/**
+ * Steam API OpenID.
+ */
+passport.use(new OpenIDStrategy({
+  apiKey: process.env.STEAM_KEY,
+  providerURL: 'http://steamcommunity.com/openid',
+  returnURL: 'http://localhost:3000/auth/steam/callback',
+  realm: 'http://localhost:3000/',
+  stateless: true
+}, (identifier, done) => {
+  const steamId = identifier.match(/\d+$/)[0];
+  const profileURL = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM_KEY}&steamids=${steamId}`;
+
+  User.findOne({ steam: steamId }, (err, existingUser) => {
+    if (existingUser) return done(err, existingUser);
+    request(profileURL, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const data = JSON.parse(body);
+        const profile = data.response.players[0];
+
+        const user = new User();
+        user.steam = steamId;
+        user.email = `${steamId}@steam.com`; // steam does not disclose emails, prevent duplicate keys
+        user.tokens.push({ kind: 'steam', accessToken: steamId });
+        user.profile.name = profile.personaname;
+        user.profile.picture = profile.avatarmedium;
+        user.save((err) => {
+          done(err, user);
+        });
+      } else {
+        done(error, null);
+      }
+    });
+  });
+}));
